25c0c2d16884749d91c9c918f40777f740a3fe09
swapnilsinha07 <32387232+swapnilsinha07@users.noreply.github.com>
2019-08-02 23:36:30 +0530

    update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,15 +1,15 @@
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
-  // var seconds = Math.floor((t / 1000) % 60);
+  var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (94 * 60 * 60 * 24));
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
   return {
     'total': t,
     'days': days,
     'hours': hours,
-    'minutes': minutes
-    // 'seconds': seconds
+    'minutes': minutes,
+    'seconds': seconds
   };
 }
 
@@ -18,7 +18,7 @@ function initializeClock(id, endtime) {
   var daysSpan = clock.querySelector('.days');
   var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
-  // var secondsSpan = clock.querySelector('.seconds');
+  var secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
@@ -26,7 +26,7 @@ function initializeClock(id, endtime) {
     daysSpan.innerHTML = t.days;
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    // secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
@@ -37,5 +37,6 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 30 * 24 * 60 * 60 * 1000);
+
+var deadline='August 20 2019 23:59:59 GMT+0530';
 initializeClock('clockdiv', deadline);
